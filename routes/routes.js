1166f2c7b14cf4dffe0705c0b1135e5d7ff7b1ea
Aktarul Islam <aktarul857@gmail.com>
2017-10-01 04:30:01 +0600

    twitter authentication completed

--- routes/routes.js
+++ routes/routes.js
@@ -39,7 +39,7 @@ module.exports = function(app, passport) {
         res.redirect('/');
     });
 
-    app.get('/auth/google', passport.authenticate('google', {scope : ['profile' , 'email']}));
+    app.get('/auth/google', passport.authenticate('google', { scope : ['profile' , 'email']}));
 
     app.get('/auth/google/callback', passport.authenticate('google', {
         successRedirect : '/profile',
@@ -50,6 +50,13 @@ module.exports = function(app, passport) {
     app.get('/auth/facebook', passport.authenticate('facebook' , { scope : 'email'}));
 
     app.get('/auth/facebook/callback', passport.authenticate('facebook',{
+        successRedirect : '/profile',
+        failureRedirect : '/'
+    }));
+
+    app.get('/auth/twitter', passport.authenticate('twitter'));
+
+    app.get('/auth/twitter/callback', passport.authenticate('twitter', {
         successRedirect : '/profile',
         failureRedirect : '/'
     }));
